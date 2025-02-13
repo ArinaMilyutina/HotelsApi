603abdafb7210074b0f86e75b4d5255db35f2c04
Arina <arina.milyutina.0005@gmail.com>
2025-02-13 22:33:33 +0300

    added package contacts and corrected regex(added whitespace)

--- src/main/java/com/example/hotelsapi/dto/contacts/ContactsRequest.java
+++ src/main/java/com/example/hotelsapi/dto/contacts/ContactsRequest.java
@@ -1,4 +1,4 @@
-package com.example.hotelsapi.dto;
+package com.example.hotelsapi.dto.contacts;
 
 import lombok.*;
 
@@ -14,6 +14,6 @@ public class ContactsRequest {
     @Size(min = 3, max = 254, message = "Email must be between 3 and 254 characters")
     @Email(message = "Invalid email format")
     private String email;
-    @Pattern(regexp = "^\\+\\d[\\d-]{0,19}\\d$", message = "Invalid phone number format. Must start with '+' and contain 1 to 19 digits and hyphens.")
+    @Pattern(regexp = "^\\+\\d[\\d-\\s]{0,19}\\d$", message = "Invalid phone number format. Must start with '+', contain 1 to 19 digits, hyphens, and spaces.")
     private String phone;
 }
