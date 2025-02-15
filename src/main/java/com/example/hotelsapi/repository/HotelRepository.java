bc6bfde96592837037e24698aac8bfc9a0b91a07
Arina <arina.milyutina.0005@gmail.com>
2025-02-15 22:53:13 +0300

    added 4 methods(count hotels by parameters(brand, city, county, amenities)

--- src/main/java/com/example/hotelsapi/repository/HotelRepository.java
+++ src/main/java/com/example/hotelsapi/repository/HotelRepository.java
@@ -1,11 +1,14 @@
 package com.example.hotelsapi.repository;
 
 
+import com.example.hotelsapi.dto.hotel.HotelCountRequest;
 import com.example.hotelsapi.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,5 +16,13 @@ public interface HotelRepository extends JpaRepository<Hotel, Long>, JpaSpecific
     Optional<Hotel> findHotelByContacts_Phone(String phone);
 
     Optional<Hotel> findHotelByContacts_Email(String email);
+    @Query("SELECT new com.example.hotelsapi.dto.hotel.HotelCountRequest(h.brand, COUNT(h)) FROM Hotel h GROUP BY h.brand")
+    List<HotelCountRequest> countHotelByBrand();
+    @Query("SELECT new com.example.hotelsapi.dto.hotel.HotelCountRequest(a.city, COUNT(h)) FROM Hotel h JOIN h.address a GROUP BY a.city")
+    List<HotelCountRequest> countHotelByAddress_City();
+    @Query("SELECT new com.example.hotelsapi.dto.hotel.HotelCountRequest(a.county, COUNT(h)) FROM Hotel h JOIN h.address a GROUP BY a.county")
+    List<HotelCountRequest> countHotelByAddress_County();
+    @Query("SELECT new com.example.hotelsapi.dto.hotel.HotelCountRequest(CAST(a as string), COUNT(h)) FROM Hotel h JOIN h.amenities a GROUP BY a")
+    List<HotelCountRequest> countHotelByAmenities();
 
 }
